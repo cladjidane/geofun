d7302d0420ef507e26281e213149adc40ab932f9
Canu Fabien <fabien.canu@gmail.com>
2015-03-02 08:47:08 +0100

    Suppression Key handler
    Suppression des gestionnaires d’événements liés au clavier. Inutile
    dans notre cas

--- app.js
+++ app.js
@@ -8,31 +8,13 @@
   template.responsiveWidth = '900px';
   
   template.addEventListener('template-bound', function(e) {
-    var keys = document.querySelector('#keys');
-
-    // Allow selecting pages by num keypad. Dynamically add
-    // [1, template.pages.length] to key mappings.
-    var keysToAdd = Array.apply(null, template.pages).map(function(x, i) {
-      return i + 1;
-    }).reduce(function(x, y) {
-      return x + ' ' + y;
-    });
-    keys.keys += ' ' + keysToAdd;
 
     this.route = this.route || DEFAULT_ROUTE; // Select initial route.
+
   });
   
   template.nextStep = function(e, detail, sender) {
     var pages = document.querySelector('#pages');
-
-    console.log('ok');
-
-    // Select page by num key. 
-    var num = parseInt(detail.key);
-    if (!isNaN(num) && num <= this.pages.length) {
-      pages.selectIndex(num - 1);
-      return;
-    }
 
     pages.selectNext();
 
@@ -41,40 +23,8 @@
   template.prevStep = function(e, detail, sender) {
     var pages = document.querySelector('#pages');
 
-    // Select page by num key. 
-    var num = parseInt(detail.key);
-    if (!isNaN(num) && num <= this.pages.length) {
-      pages.selectIndex(num - 1);
-      return;
-    }
-
     pages.selectPrevious();
 
-  };
-  
-  template.keyHandler = function(e, detail, sender) {
-    var pages = document.querySelector('#pages');
-
-    // Select page by num key. 
-    var num = parseInt(detail.key);
-    if (!isNaN(num) && num <= this.pages.length) {
-      pages.selectIndex(num - 1);
-      return;
-    }
-
-    switch (detail.key) {
-      case 'left':
-      case 'up':
-        pages.selectPrevious();
-        break;
-      case 'right':
-      case 'down':
-        pages.selectNext();
-        break;
-      case 'space':
-        detail.shift ? pages.selectPrevious() : pages.selectNext();
-        break;
-    }
   };
   
   template.cyclePages = function(e, detail, sender) {
